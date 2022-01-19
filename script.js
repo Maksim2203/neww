0a2e91a3bdc3617b8017ee79a9203af6e5261374
Maksim2203 <98012684+Maksim2203@users.noreply.github.com>
2022-01-19 15:37:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+'use strict'
+
+// function one(x) {
+//     function two() {
+//         const a = +prompt("Введите число")
+//         x--; 
+
+//         console.log(a);
+//         console.log(x);
+
+//         if (a !== x) {
+//             two()
+//         }
+
+//     }
+//     two()
+// }
+// one(10)
+// function counter(n) {
+//     return function (x) {
+//         return x + n
+//     }
+// }
+// let sum = counter(1)
+// console.log(sum(15));
+// console.log(sum(20));
+// function pathGenerator(url){
+//     return function(imageName){
+//         return url + imageName
+//     }
+// }
+// let urlToIcons = pathGenerator
+// let urlToImage= pathGenerator
 function one(sekretNumber) {
     return function two() {
         console.log(secretNumber);
