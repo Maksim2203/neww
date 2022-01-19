894b72d0973b05faa8418d3821563269ba5278d8
Maksim2203 <98012684+Maksim2203@users.noreply.github.com>
2022-01-19 16:23:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,82 @@
-'use strict'
+'use strict';
 
-// function one(x) {
-//     function two() {
-//         const a = +prompt("Введите число")
-//         x--; 
+// function one(x){
+// function two(){
+// const a = +prompt("Введите число");
+// x--;
 
-//         console.log(a);
-//         console.log(x);
+// console.log(a);
+// console.log(x);
 
-//         if (a !== x) {
-//             two()
+// if (a !== x){
+// two();
 //         }
+//     }
 
-//     }
-//     two()
+// two();
+
 // }
-// one(10)
-// function counter(n) {
-//     return function (x) {
-//         return x + n
-//     }
-// }
-// let sum = counter(1)
+
+// one(10);
+
+// function counter(n){
+// return function(x){
+// return x + n;
+//     };
+// };
+
+// let sum = counter(1);
 // console.log(sum(15));
 // console.log(sum(20));
+
 // function pathGenerator(url){
-//     return function(imageName){
-//         return url + imageName
+// return function (imageName){
+// return url + imageName;
 //     }
 // }
-// let urlToIcons = pathGenerator
-// let urlToImage= pathGenerator
-function one(sekretNumber) {
-    return function two() {
+
+
+// let x = Math.floor(Math.random()*11);
+// console.log(x);
+
+// function guessNumber(){
+// let a = +prompt('Попробуйте угадать число: ');
+// if (x !== a){
+// guessNumber(x);
+// } else{
+// alert('Ура, вы победили!')
+//     }
+// }
+
+// guessNumber(x);
+
+
+
+function one(){
+    return function two(secretNumber){
         console.log(secretNumber);
-        const userNumber = +prompt('Введите число')
-        if (isNaN(userNumber)) {
-            alert('Введите чило');
-            two();
-        } else if (userNumber == false) {
+        const userNumber = +prompt("Введите число");
+        if (isNaN(userNumber)){
+            alert('Введите число!');
+            two(secretNumber);
+        } else if (userNumber == false){
             return
         }
 
-        if (userNumber != secretNumber) {
-            if (secretNumber < userNumber) {
-                alert('')
-                two();
-            } else if (secretNumber > userNumber) {
-                alert('');
-                two();
+        if (userNumber != secretNumber){
+            if (secretNumber < userNumber){
+                alert('Загаданное число меньше!');
+                two(secretNumber);
+            } else if (secretNumber >> userNumber){
+                alert('Загаданное число больше!');
+                two(secretNumber);
             }
-
+            
         } else {
-            alert('')
+            alert('Вы победили')
             return
         }
     }
 }
+constnumber = one();
+number(Math.floor(Math.random()*101));
